e37954432d34e724f7f241481e6f76c9f66452d6
rohit sharma <ansh.sds@gmail.com>
2021-09-16 22:33:30 +0530

    Added search page and updated login page

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -2,4 +2,14 @@ package com.qa.pages;
 
 public class LoginPage {
 
+	
+	public void login()
+	{
+		System.out.println("THis is login page");
+	}
+	
+	public void Signup()
+	{
+		System.out.println("THis is login page");
+	} 
 }
